0f009580f9fe3866351567aaa5ea9f312a98149a
rebeljelly2147 <c4coder3377@gmail.com>
2025-01-26 14:24:44 +0530

    Add viewport meta tag and refactor Navbar component for improved responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,102 +14,105 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <nav
-      className={`${styles.paddingX} py-5 w-full flex items-center fixed top-0 z-20 bg-primary`}
-    >
-      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-        <Link
-          to={navigationPaths.home}
-          className="flex items-center gap-2"
-          onClick={() => {
-            setActive("");
-            window.scrollTo(0, 0);
-          }}
-        >
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer flex">
-            {/* personalInfo is an object containing my name and role */}
-            {/* Here I will add my name and &nbsp will add a space after my name */}
-            {personalInfo.name} &nbsp;
-            <span className="lg:block hidden">
-              {" "}
-              | &nbsp; {personalInfo.role}
-            </span>
-          </p>
-        </Link>
-
-        <ul className="list-none hidden sm:flex flex-row gap-10">
-          {navLinks.map((link) => (
-            // Here I am mapping over the navLinks array and returning a list item for each link in the array
-            <li
-              key={link.id}
-              className={`${
-                active === link.title ? "text-white" : "text-secondary"
-              } text-[18px] font-medium cursor-pointer hover:text-white`}
-              onClick={() => setActive(link.title)}
-            >
-              <a href={`#${link.id}`}>{link.title}</a>
-            </li>
-          ))}
-
-          <li
-            className={`text-secondary text-[18px] font-medium cursor-pointer hover:text-white`}
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <nav
+        className={`${styles.paddingX} py-5 w-full flex items-center fixed top-0 z-20 bg-primary`}
+      >
+        <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+          <Link
+            to={navigationPaths.home}
+            className="flex items-center gap-2"
+            onClick={() => {
+              setActive("");
+              window.scrollTo(0, 0);
+            }}
           >
-            <a
-              href={publicUrls.resume}
-              download="Abhishek_Singh_Resume.pdf"
-              target="_blank"
-            >
-              Resume
-            </a>
-          </li>
-        </ul>
+            <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+            <p className="text-white text-[18px] font-bold cursor-pointer flex">
+              {/* personalInfo is an object containing my name and role */}
+              {/* Here I will add my name and &nbsp will add a space after my name */}
+              {personalInfo.name} &nbsp;
+              <span className="lg:block hidden">
+                {" "}
+                | &nbsp; {personalInfo.role}
+              </span>
+            </p>
+          </Link>
 
-        <div className="sm:hidden flex flex-1 justify-end items-center">
-          <img
-            src={toggle ? close : menu}
-            alt="menu"
-            className="w-[28px] h-[28px] object-contain cursor-pointer"
-            onClick={() => setToggle(!toggle)}
-          />
-
-          <div
-            className={`${
-              !toggle ? "hidden" : "flex"
-            } absolute top-20 right-0 black-gradient mx-4 my-2 p-6 rounded-xl z-10 min-w-[140px]`}
-          >
-            <ul className="list-none flex flex-col gap-4 justify-end items-start">
-              {navLinks.map((link) => (
-                <li
-                  key={link.id}
-                  className={`${
-                    active === link.title ? "text-white" : "text-secondary"
-                  } text-[18px] font-medium cursor-pointer hover:text-white`}
-                  onClick={() => {
-                    setActive(link.title);
-                    setToggle(!toggle);
-                  }}
-                >
-                  <a href={`#${link.id}`}>{link.title}</a>
-                </li>
-              ))}
-
+          <ul className="list-none hidden sm:flex flex-row gap-10">
+            {navLinks.map((link) => (
+              // Here I am mapping over the navLinks array and returning a list item for each link in the array
               <li
-                className={`text-secondary text-[18px] font-medium cursor-pointer hover:text-white`}
+                key={link.id}
+                className={`${
+                  active === link.title ? "text-white" : "text-secondary"
+                } text-[18px] font-medium cursor-pointer hover:text-white`}
+                onClick={() => setActive(link.title)}
               >
-                <a
-                  href={publicUrls.resume}
-                  download="Abhishek_Singh_Resume.pdf"
-                  target="_blank"
-                >
-                  Resume
-                </a>
+                <a href={`#${link.id}`}>{link.title}</a>
               </li>
-            </ul>
+            ))}
+
+            <li
+              className={`text-secondary text-[18px] font-medium cursor-pointer hover:text-white`}
+            >
+              <a
+                href={publicUrls.resume}
+                download="Abhishek_Singh_Resume.pdf"
+                target="_blank"
+              >
+                Resume
+              </a>
+            </li>
+          </ul>
+
+          <div className="sm:hidden flex flex-1 justify-end items-center">
+            <img
+              src={toggle ? close : menu}
+              alt="menu"
+              className="w-[28px] h-[28px] object-contain cursor-pointer"
+              onClick={() => setToggle(!toggle)}
+            />
+
+            <div
+              className={`${
+                !toggle ? "hidden" : "flex"
+              } absolute top-20 right-0 black-gradient mx-4 my-2 p-6 rounded-xl z-10 min-w-[140px]`}
+            >
+              <ul className="list-none flex flex-col gap-4 justify-end items-start">
+                {navLinks.map((link) => (
+                  <li
+                    key={link.id}
+                    className={`${
+                      active === link.title ? "text-white" : "text-secondary"
+                    } text-[18px] font-medium cursor-pointer hover:text-white`}
+                    onClick={() => {
+                      setActive(link.title);
+                      setToggle(!toggle);
+                    }}
+                  >
+                    <a href={`#${link.id}`}>{link.title}</a>
+                  </li>
+                ))}
+
+                <li
+                  className={`text-secondary text-[18px] font-medium cursor-pointer hover:text-white`}
+                >
+                  <a
+                    href={publicUrls.resume}
+                    download="Abhishek_Singh_Resume.pdf"
+                    target="_blank"
+                  >
+                    Resume
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 
