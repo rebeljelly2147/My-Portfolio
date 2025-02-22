b4a468ed3e5e17aea2f8705ba84ebe2636474922
Abhishek Singh <c4coder3377@gmail.com>
2025-02-22 21:12:33 +0530

    Updated heading of experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -63,7 +63,7 @@ const Experience = () => {
     <>
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>What I have done so far</p>
-        <h2 className={styles.sectionHeadText}>Work Experience.</h2>
+        <h2 className={styles.sectionHeadText}>Journey Till Now...</h2>
         {/* <h2 className={styles.sectionHeadText}>Work Experience.</h2> */}
       </motion.div>
 
